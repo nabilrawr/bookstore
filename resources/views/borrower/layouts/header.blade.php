eb9b8cd6f6e2d1e8e7d2217888e8cebc8076371c
Muhammad Fuad Bin Hamzah <fuadfuza31@gmail.com>
2022-06-04 12:26:46 +0800

    Separate user login di homeController

--- resources/views/borrower/layouts/header.blade.php
+++ resources/views/borrower/layouts/header.blade.php
@@ -451,6 +451,16 @@
             </li>
             </ul>
             </div>
+
+                <a class="dropdown-item" href="{{ route('logout') }}"
+                   onclick="event.preventDefault();
+                                 document.getElementById('logout-form').submit();">
+                    {{ __('Logout') }}
+                </a>
+
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                    @csrf
+                </form>
       </nav>
     </header>
      <!--end top header-->
