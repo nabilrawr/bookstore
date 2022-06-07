ee12f27dc3053117de35cd98ebf2c704c8143a2d
izzaty-github <nurhaziqahizzatymohdisa@gmail.com>
2022-06-07 09:21:53 +0800

    fix

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -6,11 +6,7 @@
                        alt="logo icon">
                </div>
                <div>
-<<<<<<< HEAD
-                   <h4 class="logo-text">Bookstore</h4>
-=======
                    <h4 class="logo-text">BRMS</h4>
->>>>>>> nabil
                </div>
                <div class="toggle-icon ms-auto"> <i class="bi bi-list"></i>
                </div>
@@ -34,7 +30,6 @@
                    </a>
                </li>
                <li>
-<<<<<<< HEAD
                    <a href="{{ route('profile-edit', Auth::user()->id) }}">
                        <div class="parent-icon"><i class="bi bi-house-fill"></i>
                        </div>
@@ -44,14 +39,12 @@
                @if (Auth::user()->role == 1)
                    <li class="menu-label">Account</li>
 
-=======
-                <a href="{{ route('borrower.book-list') }}">
-                    <div class="parent-icon"><i class="bi bi-card-list"></i>
-                    </div>
-                    <div class="menu-title">List of Book</div>
-                </a>
-            </li>
->>>>>>> nabil
+                   <a href="{{ route('borrower.book-list') }}">
+                       <div class="parent-icon"><i class="bi bi-card-list"></i>
+                       </div>
+                       <div class="menu-title">List of Book</div>
+                   </a>
+                   </li>
                    <li>
                        <a href="{{ route('borrower.index-booking') }}">
                            <div class="parent-icon"><i class="bi bi-journal-bookmark-fill"></i>
@@ -59,81 +52,80 @@
                            <div class="menu-title">Rental Record</div>
                        </a>
                    </li>
-<<<<<<< HEAD
-=======
-               @if(Auth::user()->role == 1)
-               <li class="menu-label">Account</li>
->>>>>>> nabil
 
-                   <li>
-                       <a href="{{ route('borrower.index-booking') }}">
-                           <div class="parent-icon"><i class="bi bi-file-earmark-break-fill"></i>
-                           </div>
-                           <div class="menu-title">Active Rental</div>
-                       </a>
-                   </li>
-               @endif
-               @if (Auth::user()->role == 3 || Auth::user()->role == 2)
-                   <li class="menu-label">Book Rent</li>
+                   @if (Auth::user()->role == 1)
+                       <li class="menu-label">Account</li>
 
-                   <li>
-                       <a href="{{ route('staff.rent-record') }}">
-                           <div class="parent-icon"><i class="bi bi-basket2-fill"></i>
-                           </div>
-                           <div class="menu-title">Rent</div>
-                       </a>
-                   </li>
+                       <li>
+                           <a href="{{ route('borrower.index-booking') }}">
+                               <div class="parent-icon"><i class="bi bi-file-earmark-break-fill"></i>
+                               </div>
+                               <div class="menu-title">Active Rental</div>
+                           </a>
+                       </li>
+                   @endif
+                   @if (Auth::user()->role == 3 || Auth::user()->role == 2)
+                       <li class="menu-label">Book Rent</li>
 
+                       <li>
+                           <a href="{{ route('staff.rent-record') }}">
+                               <div class="parent-icon"><i class="bi bi-basket2-fill"></i>
+                               </div>
+                               <div class="menu-title">Rent</div>
+                           </a>
+                       </li>
 
-                   <li class="menu-label">Maintenance</li>
-                   @if (Auth::user()->role == 3)
+                       <li class="menu-label">Maintenance</li>
+                       @if (Auth::user()->role == 3)
+                           <li>
+                               <a href="javascript:;" class="has-arrow">
+                                   <div class="parent-icon"><i class="bi bi-book-fill"></i>
+                                   </div>
+                                   <div class="menu-title">User</div>
+                               </a>
+                               <ul>
+                                   <li> <a href="{{ route('admin.user-list') }}"><i class="bi bi-circle"></i>User
+                                           List</a>
+                                   </li>
+                               </ul>
+                               <ul>
+                                   <li> <a href="{{ route('admin.create-role') }}"><i
+                                               class="bi bi-circle"></i>Assign
+                                           Role</a>
+                                   </li>
+                               </ul>
+                           </li>
+                       @endif
                        <li>
                            <a href="javascript:;" class="has-arrow">
                                <div class="parent-icon"><i class="bi bi-book-fill"></i>
                                </div>
-                               <div class="menu-title">User</div>
+                               <div class="menu-title">Book</div>
                            </a>
                            <ul>
-                               <li> <a href="{{ route('admin.user-list') }}"><i class="bi bi-circle"></i>User List</a>
+                               <li> <a href="{{ route('book.index') }}"><i class="bi bi-circle"></i>Book List</a>
+                               </li>
+                               <li> <a href="{{ route('book.create') }}"><i class="bi bi-circle"></i>Book Add</a>
                                </li>
                            </ul>
+                       </li>
+
+                       <li>
+                           <a href="javascript:;" class="has-arrow">
+                               <div class="parent-icon"><i class="bi bi-basket2-fill"></i>
+                               </div>
+                               <div class="menu-title">Category</div>
+                           </a>
                            <ul>
-                               <li> <a href="{{ route('admin.create-role') }}"><i class="bi bi-circle"></i>Assign
-                                       Role</a>
+                               <li> <a href="{{ route('category.index') }}"><i class="bi bi-circle"></i>Category
+                                       List</a>
+                               </li>
+                               <li> <a href="{{ route('category.create') }}"><i class="bi bi-circle"></i>Category
+                                       Add</a>
                                </li>
                            </ul>
                        </li>
                    @endif
-                   <li>
-                       <a href="javascript:;" class="has-arrow">
-                           <div class="parent-icon"><i class="bi bi-book-fill"></i>
-                           </div>
-                           <div class="menu-title">Book</div>
-                       </a>
-                       <ul>
-                           <li> <a href="{{ route('book.index') }}"><i class="bi bi-circle"></i>Book List</a>
-                           </li>
-                           <li> <a href="{{ route('book.create') }}"><i class="bi bi-circle"></i>Book Add</a>
-                           </li>
-                       </ul>
-                   </li>
-
-                   <li>
-                       <a href="javascript:;" class="has-arrow">
-                           <div class="parent-icon"><i class="bi bi-basket2-fill"></i>
-                           </div>
-                           <div class="menu-title">Category</div>
-                       </a>
-                       <ul>
-                           <li> <a href="{{ route('category.index') }}"><i class="bi bi-circle"></i>Category
-                                   List</a>
-                           </li>
-                           <li> <a href="{{ route('category.create') }}"><i class="bi bi-circle"></i>Category
-                                   Add</a>
-                           </li>
-                       </ul>
-                   </li>
-               @endif
 
            </ul>
            <!--end navigation-->
